96c320eb9e1212b220e6684a625ea57d07e196ac
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2025-06-16 15:32:13 +0800

    Delete src directory

--- src/base.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "core.hpp"
-#include "process.hpp"
-#include "node.hpp"
-
-#ifdef BASE_INCLUDING_GAME_OBJECTS
-#include "game.hpp"
-#endif
--- src/game.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "game/game_object.hpp"
-#include "game/game_node.hpp"
-#include "game/scene_tree.hpp"
