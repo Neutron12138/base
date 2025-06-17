26936e5f4473291e1446f9d7f813bb0b1c63f056
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2025-06-17 14:32:53 +0800

    Delete base directory

--- base/node.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "node/node.hpp"
-// #include "node/tree_node.hpp"
-// #include "node/node_tree.hpp"
-
-#include "node/node.cpp"
-// #include "node/tree_node.cpp"
-// #include "node/node_tree.cpp"
